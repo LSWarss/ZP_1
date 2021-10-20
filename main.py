bb77ae11c32cfadfe741fd7bf0dbdb7df4c66dae
LSWarss <ls.warss98@gmail.com>
2021-10-20 15:43:34 +0200

    Added typehints and doc strings for the functions

--- main.py
+++ main.py
@@ -1,39 +1,72 @@
+from typing import * 
 
-def printer(namesArray):
+def printer(namesArray : list[str]):
+    """Prints names from the given array
+
+    Args:
+        namesArray (list[str]): List of strings preferable names
+    """
     for name in namesArray:
         print(name) 
 
-printer(["Kasia", "Basia", "Michał", "Piotr", "Kot", "Ziobro"])
+def doubler(numbers : Union[list[int], list[float]]) -> Union[list[int], list[int]]:
+    """Returns given array with all numbers multiplied by 2
 
-def doubler(numbers):
+    Args:
+        numbers (Union[list[int], list[float]]): Array of numbers
+
+    Returns:
+        Union[list[int], list[int]]: Array of numbers muliplied by 2
+    """
     returnArray = []
     for number in numbers:
         returnArray.append(number * 2)
     return returnArray
 
-print(doubler([1,2,3,4,5,6]))
+def arrayDoubler(numbers : Union[list[int], list[float]]) -> Union[list[int], list[int]]:
+    """Returns given array with all numbers multiplied by 2
 
-def arrayDoubler(numbers):
+    Args:
+        numbers (Union[list[int], list[float]]): Array of numbers
+
+    Returns:
+        Union[list[int], list[int]]: Array of numbers muliplied by 2
+    """
     return [(number * 2) for number in numbers]
 
-print(arrayDoubler([1,2,3,4,5,6]))
+def oddInArray(numbers : Union[list[int], list[float]]):
+    """Prints out odd numbers in given array
 
-def oddInArray(numbers): 
+    Args:
+        numbers (Union[list[int], list[float]]): Array of number
+    """
     for number in numbers: 
         if number % 2 != 0:
             print(number)
 
-def evenInArray(numbers): 
+def evenInArray(numbers : Union[list[int], list[float]]):
+    """Prints out even numbers in given array
+
+    Args:
+        numbers (Union[list[int], list[float]]): Array of number
+    """
     for number in numbers: 
         if number % 2 == 0:
             print(number)
 
-print(oddInArray([1,2,3,4,5,6,7,8,9,10]))
-print(evenInArray([1,2,3,4,5,6,7,8,9,10]))
+def everySecondFromArray(numbers : Union[list[int], list[float]]):
+    """Prints out every second number in given array
 
-def everySecondFromArray(numbers):
+    Args:
+        numbers (Union[list[int], list[float]]): [
+    """
     for i,n in enumerate(numbers):
         if i % 2 == 0:
             print(n)
 
+printer(["Kasia", "Basia", "Michał", "Piotr", "Kot", "Ziobro"])
+print(arrayDoubler([1,2,3,4,5,6]))
+print(doubler([1,2,3,4,5,6]))
+print(oddInArray([1,2,3,4,5,6,7,8,9,10]))
+print(evenInArray([1,2,3,4,5,6,7,8,9,10]))
 print(everySecondFromArray([1,3,5,123,23,23,56,1]))
